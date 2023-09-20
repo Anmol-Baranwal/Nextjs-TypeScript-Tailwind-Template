21f31691e668eecba7157a9b08e70c49af0a0c61
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-09-21 01:46:16 +0530

    feat: add mandatory things and update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
