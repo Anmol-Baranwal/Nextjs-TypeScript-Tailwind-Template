174b6bad26df3e261e8b21ddb61b33b8a9705c1b
Anmol Baranwal <anmolbaranwal119@gmail.com>
2024-02-17 01:08:28 +0530

    chore: add prettier and strict the environment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com"],
+    domains: ['images.unsplash.com'],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
